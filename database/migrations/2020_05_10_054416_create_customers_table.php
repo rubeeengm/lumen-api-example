90480727820a761b1deda83b17a943f09a2d40f9
Ruben Malaga <rubeeengm@outlook.com>
2020-05-10 01:31:34 -0500

    Migrations
    + create migration
    + create table
    + add column
    + update column
    + soft delete
    + migration rollback

---
+++ database/migrations/2020_05_10_054416_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('last_name',100);
+            $table->string('address',200)->nullable();
+            $table->string('identification',20)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
